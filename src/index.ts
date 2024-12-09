bdba2c23ddb347d10ca8f1465882905f863443ea
Julia Roxana Natera <103115280+roxanatera@users.noreply.github.com>
2024-12-09 15:30:21 +0100

    Ajustes en rutas y Swagger para Render

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors'; // Importa CORS
+import cors from 'cors';
 import productRouter from './routes/productRoutes';
 import swaggerUi from 'swagger-ui-express';
-import path from 'path'; // Importa path para resolver las rutas
+import path from 'path';
 
 dotenv.config();
 
@@ -17,26 +17,26 @@ if (!MONGODB_URI) {
     process.exit(1);
 }
 
-// Configuración del Middleware
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 app.use('/api/products', productRouter);
 
-// Configuración de Swagger UI usando swagger.json
+// Configuración de Swagger
+let swaggerPath = process.env.NODE_ENV === 'production'
+    ? path.resolve(__dirname, 'swagger.json')
+    : path.resolve(__dirname, '../swagger.json');
+
 try {
-    
-    const swaggerDocument = require(path.resolve(__dirname, '../swagger.json')); // Carga el archivo JSON desde la carpeta dist o src
+    const swaggerDocument = require(swaggerPath);
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     console.log('Swagger documentation is ready at /api-docs');
 } catch (error) {
     console.error('Error loading Swagger documentation:', error);
 }
 
-
 mongoose.connect(MONGODB_URI)
     .then(() => {
         console.log('MongoDB connected');
-        // Inicia el servidor solo si la conexión a la base de datos fue exitosa
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
             console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
